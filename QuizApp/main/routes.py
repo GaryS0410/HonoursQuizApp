a147ee27629c394236efcd5380a8244dab240c69
GaryS0410 <garyjsmith0410@gmail.com>
2023-02-26 13:50:15 +0000

    Inital commit

---
+++ QuizApp/main/routes.py
@@ -0,0 +1,17 @@
+from flask import render_template
+
+from QuizApp.main import bp
+
+@bp.route('/')
+def index():
+    questions = [
+        "Little interest or pleasure in doing things?",
+        "Feeling down, depressed, or hopeless?",
+        "Trouble falling or staying asleep, or sleeping too much?",
+        "Feeling tired or having little energy?",
+        "Poor appetite or overeating?",
+        "Feeling bad about yourself — or that you are a failure or have let yourself or your family down?",
+        "Trouble concentrating on things, such as reading the newspaper or watching television?",
+        "Moving or speaking so slowly that other people could have noticed?"
+    ]
+    return render_template('index.html', questions = questions)
